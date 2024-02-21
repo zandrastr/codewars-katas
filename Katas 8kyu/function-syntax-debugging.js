f32666369ffe09ae40f49bcfb3b932de281da676
Sandra S <zandrastr@hotmail.com>
2024-02-21 16:15:39 +0000

    add function-syntax-debugging solution

---
+++ Katas 8kyu/function-syntax-debugging.js
@@ -0,0 +1,21 @@
+// --------------------------------------------------------------
+// DESCRIPTION:
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. 
+// Help them find their mistakes and fix them:
+
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+// --------------------------------------------------------------
+// MY SOLUTION 1:
+function main (verb, noun) {
+    return verb + noun
+}
+
+// --------------------------------------------------------------
+// MY SOLUTION 2:
+const main = (verb, noun) => {
+    return verb + noun
+}
