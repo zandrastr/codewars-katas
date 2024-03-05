ccb259fe52e45dcce53a08bc273f691d7acee580
Sandra S <zandrastr@hotmail.com>
2024-03-05 12:40:47 +0000

    add reversed-strings solution

---
+++ Katas 8kyu/reversed-strings.js
@@ -0,0 +1,12 @@
+// --------------------------------------------------------------
+// DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// --------------------------------------------------------------
+// MY SOLUTION :
+function solution(str){
+    return str.split('').reverse().join('');
+}
