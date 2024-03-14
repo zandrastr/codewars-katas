a3ff2b464cbaea0cca3c914a2fc604f9fef70be6
Sandra S <zandrastr@hotmail.com>
2024-03-14 17:14:18 +0000

    add double-integer solution

---
+++ Katas 8kyu/double-integer.js
@@ -0,0 +1,9 @@
+// --------------------------------------------------------------
+// DESCRIPTION:
+//You need to double the integer (i) and return it.
+
+// --------------------------------------------------------------
+// MY SOLUTION:
+function doubleInteger(i) {
+  return i * 2;
+}
